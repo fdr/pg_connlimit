11ab13aed46ad25c6440211fef10a773a219ac18
Daniel Farina <daniel@heroku.com>
2013-11-10 19:55:45 -0800

    Make alphanum check more terse
    Also allows an underscore, which seems harmless.
    
    Result of code review by:
    
    Peter Geoghegan <pg@heroku.com>

--- pg_connlimit.c
+++ pg_connlimit.c
@@ -61,7 +61,6 @@ static char *connlimitDirectory = NULL;
 /* Internal function definitions. */
 static void client_auth_hook(Port *port, int status);
 static void enforce_limit(char *rolname);
-static bool isalphanum(char *str);
 
 /*
  * _PG_init()			- library load-time initialization
@@ -113,34 +112,6 @@ client_auth_hook(Port *port, int status)
 
 }
 
-/* Check if the given NUL-terminated string is ASCII-alphanumeric. */
-static bool
-isalphanum(char *str)
-{
-	char c;
-
-	c = *str;
-
-	/* An empty string is considered non-alphanumeric */
-	if (c == '\0')
-		return false;
-
-	do
-	{
-		if ((c >= '0' && c <= '9') ||
-			(c >= 'A' && c <= 'Z') ||
-			(c >= 'a' && c <= 'z'))
-		{
-			str += 1;
-			c = *str;
-		}
-		else
-			return false;
-	} while (c != '\0');
-
-	return true;
-}
-
 static void
 enforce_limit(char *rolname)
 {
@@ -166,7 +137,8 @@ enforce_limit(char *rolname)
 	 * Role is not alphanumeric.  Don't let it be used as input to open(), to
 	 * avoid traversal attacks (e.g. contained '.' characters).
 	 */
-	if (!isalphanum(rolname))
+	if (strspn(rolname, "abcdefghijklmnopqrstuvwxyz0123456789_") !=
+		strlen(rolname))
 		return;
 
 	/* Compute path to probe for connection limit enforcement. */
